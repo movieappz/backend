8be1923fe4bc86ed866951931f0ff4ecdd364864
Farid Tavakoli <89019161+FaridTvK@users.noreply.github.com>
2025-10-30 16:47:59 +0100

    backend done

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ const corsOptions = {
     allowedHeaders: ["Content-Type", "Authorization"],
 };
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
